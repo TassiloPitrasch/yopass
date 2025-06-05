77d0a7d469f1b42a2d53d38eb1864dc6cdf9d3fa
Tassilo Pitrasch <60175677+TassiloPitrasch@users.noreply.github.com>
2025-06-05 11:13:35 +0200

    Dark-mode (#249)

--- website/src/shared/Header.tsx
+++ website/src/shared/Header.tsx
@@ -1,6 +1,15 @@
-import { AppBar, Toolbar, Typography, Button, Box, Link } from '@mui/material';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Box,
+  Link,
+  Stack,
+} from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { useLocation } from 'react-router-dom';
+import { ModeToggle } from './ModeToggle';
 
 export const Header = () => {
   const { t } = useTranslation();
@@ -12,7 +21,7 @@ export const Header = () => {
   const fileUpload = process.env.YOPASS_DISABLE_FILE_UPLOAD !== '1';
   // Replace home with base in first href below to force reload when clicking on logo
   return (
-    <AppBar position="static" color="transparent" sx={{ marginBottom: 4 }}>
+    <AppBar position="static" color="default" sx={{ marginBottom: 4 }}>
       <Toolbar>
         <Link href={home} color="inherit" underline="none">
           <Typography variant="h6" component="div">
@@ -33,20 +42,23 @@ export const Header = () => {
         </Link>
         {fileUpload && (
           <>
-            <Box
+            <Stack
+              direction="row"
+              gap={2}
               sx={{
                 marginLeft: 'auto',
               }}
-             >
-               <Button
-                 component={Link}
-                 href={isOnUploadPage ? home : upload}
-                 variant="contained"
-                 color="primary"
-               >
-                 {isOnUploadPage ? t('header.buttonHome') : t('header.buttonUpload')}
-               </Button>
-             </Box>
+            >
+              <ModeToggle />
+              <Button
+                component={Link}
+                href={isOnUploadPage ? home : upload}
+                variant="contained"
+                color="primary"
+              >
+                {isOnUploadPage ? t('header.buttonHome') : t('header.buttonUpload')}
+              </Button>
+            </Stack>
           </>
         )}
       </Toolbar>
